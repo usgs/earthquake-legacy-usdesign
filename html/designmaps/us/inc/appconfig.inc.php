1edd6b6b94a27778c2d5b68e96b7bb8d6a5b71fa
ehunter-usgs <ehunter@usgs.gov>
2016-10-24 10:03:55 -0600

    Update environment variable name for cross node prefix

--- html/designmaps/us/inc/appconfig.inc.php
+++ html/designmaps/us/inc/appconfig.inc.php
@@ -25,4 +25,4 @@
 
 	$APP_MAPQUEST_KEY = 'Fmjtd%7Cluub2qa7n0%2Can%3Do5-9u7a9z';
 
-	$CONFIG = array_merge(array('node' => ''), $_SERVER, $_ENV);
+	$CONFIG = array_merge(array('CROSS_NODE_PREFIX' => ''), $_SERVER, $_ENV);
